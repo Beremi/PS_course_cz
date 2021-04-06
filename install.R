83819dce6f2fd1832705f46690a747ee6f05e033
Michal <michal.beres@vsb.cz>
2021-04-06 20:05:39 +0200

    Added packages to install for binder build

--- install.R
+++ install.R
@@ -3,5 +3,11 @@ install.packages("dplyr")
 install.packages("openxlsx")
 install.packages("moments")
 
+install.packages("lawstat")
+install.packages("BSDA")
+install.packages("EnvStats")
+install.packages("binom")
+
+
 install.packages('IRkernel')
 IRkernel::installspec(user = TRUE)

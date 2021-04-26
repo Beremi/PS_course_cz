b7ff4559b2eff6a5a5e8c889383a526f758bc981
Michal <michal.beres@vsb.cz>
2021-04-26 18:20:34 +0200

    Added full CV12

--- install.R
+++ install.R
@@ -8,6 +8,7 @@ install.packages("BSDA")
 install.packages("EnvStats")
 install.packages("binom")
 install.packages("car")
+install.packages("dunn.test")
 
 #install.packages('IRkernel')
 #IRkernel::installspec(user = TRUE)
